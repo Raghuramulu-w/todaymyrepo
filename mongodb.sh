4bdd05fb40bedf84e0c2aa92cf9525e37c8d0465
raghuramulu-w <raghu0711@gmail.com>
2024-07-14 13:14:22 +0530

    hai

--- mongodb.sh
+++ mongodb.sh
@@ -30,5 +30,5 @@ dnf install mongodb-org -y &>> $LOGFILE
 VALIDATE $? "package installation"
 systemctl enable mongod
 systemctl start mongod
-sed -i 's/127.0.0.1  0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "remote bind changes"
