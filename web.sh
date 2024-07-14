48334bfc4775b58743a2a754cb4101ae06067960
raghuramulu-w <raghu0711@gmail.com>
2024-07-14 18:03:22 +0530

    hai

--- web.sh
+++ web.sh
@@ -4,7 +4,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-#MONGDB_HOST=mongodb.daws76s.fun
+MONGDB_HOST=mongodb.daws76s.fun
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
