f5869b9830ac3ef0bf40d04f536b0247f317d01c
raghuramulu-w <raghu0711@gmail.com>
2024-07-14 12:39:59 +0530

    hai

--- mongodb.sh
+++ mongodb.sh
@@ -6,16 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0 -$TIMESTAMP.log"
-echo "script executation started at $TIMESTAMP"
-if [ $ID -ne 0 ]
- then
-    echo -e "$R error you are not root user "
-    exit 1
- else
-    echo -e "$G you are root user so that you can proceed instalation $N"
-fi
-#cp mongo.repo /etc/yum.repos.d/mongo.repo
-#VALIDATE()
+CHECKING(){
  for package in $@
     do
     yum list installed $package
@@ -26,3 +17,13 @@ fi
         echo "package is already installed skipping"
     fi
  done
+}
+echo "script executation started at $TIMESTAMP"
+if [ $ID -ne 0 ]
+ then
+    echo -e "$R error you are not root user "
+    exit 1
+ else
+    echo -e "$G you are root user so that you can proceed instalation $N"
+fi
+cp mongo.repo /etc/yum.repos.d/mongo.repo
