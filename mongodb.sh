e071a078970cf6a84ee21bc0255d341a5a594e43
raghuramulu-w <raghu0711@gmail.com>
2024-07-14 12:23:17 +0530

    hai

--- mongodb.sh
+++ mongodb.sh
@@ -6,17 +6,7 @@ Y="\[33m"
 N="\[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="\tmp\$0-TIMESTAMP.log"
-VALIDATE(){
- for package in $@
-    do
-      if [ $? -ne 0 ]
-    then 
-        yum install $package &>> $LOGFILE
-    else
-        echo "package is already installed skipping"
-    fi
- done
-}
+
 
 if [ $ID -ne 0 ]
  then
@@ -26,4 +16,14 @@ if [ $ID -ne 0 ]
     echo -e "$G you are root user so that you can proceed instalation $N"
 fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE()
+#VALIDATE()
+ for package in $@
+    do
+    yum list installed $package
+      if [ $? -ne 0 ]
+    then 
+        yum install $package &>> $LOGFILE
+    else
+        echo "package is already installed skipping"
+    fi
+ done
