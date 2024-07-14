0885a3f322781f0bb2df261b55bb6b751e134835
raghuramulu-w <raghu0711@gmail.com>
2024-07-14 12:17:20 +0530

    hai

--- mongodb.sh
+++ mongodb.sh
@@ -7,22 +7,21 @@ N="\[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="\tmp\$0-TIMESTAMP.log"
 VALIDATE(){
-    for package in $@
+ for package in $@
     do
-    if [ $? -ne 0 ]
+      if [ $? -ne 0 ]
     then 
         yum install $package &>> $LOGFILE
     else
         echo "package is already installed skipping"
     fi
-    done
+ done
 }
-
 if [ $ID -ne 0 ]
-then
+ then
     echo -e "$R error you are not root user "
     exit 1
-else
+ else
     echo -e "$G you are root user so that you can proceed instalation $N"
 fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo
